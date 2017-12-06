dfc7b7ecf6728e61ba445eb865dd8a3af0aa7951
David Killian <dpkillian@icloud.com>
2017-12-06 11:32:08 -0500

    Initial commit with starting files

---
+++ key.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'Tykoyi1Q0Z5jw660UZqVXKJI6',
+  consumer_secret: 'chfAaYXflkzSOFVeF78mf9NMUAo2RPagjPamRENMOrHwsisChy',
+  access_token_key: '15471160-LUFK6IxQEC3zwUYWz48OyQHDOL5UNkIdR4mUMdhcj',
+  access_token_secret: 'ldapb8zuiX47KH5TP1EGMGTPOuhsq8S4u1bPeTSQqCWC5',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,12 @@
+
+// Load the npm package: "inquirer"
+var inquirer = require("inquirer");
+
+// Load the npm package: "node-spotify-api"
+var spotify = require("node-spotify-api");
+
+// Load the npm package: "twitter"
+var twitter = require("twitter");
+
+// Requiring our "twitterKeys" module exported from key.js
+var twitterKeys = require("./key.js");
