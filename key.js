a03dd69e052db0eb413d7e2fbee823507c68747a
David Killian <dpkillian@icloud.com>
2017-12-08 19:29:12 -0500

    Added additional code, updated files

--- key.js
+++ key.js
@@ -1,10 +1,11 @@
-console.log('twitterKeys module this is loaded!\n');
+console.log("\n------------------------------------");
+console.log("\nJavaScript module 'twitterKeys' this is loaded!");
 
 var twitterKeys = {
-  consumer_key: 'Tykoyi1Q0Z5jw660UZqVXKJI6',
-  consumer_secret: 'chfAaYXflkzSOFVeF78mf9NMUAo2RPagjPamRENMOrHwsisChy',
-  access_token_key: '15471160-LUFK6IxQEC3zwUYWz48OyQHDOL5UNkIdR4mUMdhcj',
-  access_token_secret: 'ldapb8zuiX47KH5TP1EGMGTPOuhsq8S4u1bPeTSQqCWC5',
+  consumer_key: "Tykoyi1Q0Z5jw660UZqVXKJI6",
+  consumer_secret: "chfAaYXflkzSOFVeF78mf9NMUAo2RPagjPamRENMOrHwsisChy",
+  access_token_key: "15471160-LUFK6IxQEC3zwUYWz48OyQHDOL5UNkIdR4mUMdhcj",
+  access_token_secret: "ldapb8zuiX47KH5TP1EGMGTPOuhsq8S4u1bPeTSQqCWC5",
 }
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -1,10 +1,24 @@
 
+
+// REQUEST INITIALIZATION
 // Load the npm package: "request"
 var request = require("request");
 
-// Load the npm package: "node-spotify-api"
-var spotify = require("node-spotify-api");
 
+
+// SPOTIFY INITIALIZATION
+// Load the npm package: "node-spotify-api"
+var Spotify = require("node-spotify-api");
+
+// Instantiate spotifyThis instance from Constructor Spotify
+var spotify = new Spotify({
+  id: "fadeb4504d4b444b8a2dd9b44ae7b7b7",
+  secret: "8443abef25cf4edca616bd91e7f8e6f5"
+});
+
+
+
+// TWITTER INITIALIZATION
 // Load the npm package: "twitter"
 var Twitter = require("twitter");
 
@@ -14,62 +28,108 @@ var twitterKeys = require("./key.js");
 // Create new authenticated twitter user named "client"
 var client = new Twitter(twitterKeys);
 
+
+
+// COMMAND LINE PARSING
 // Assign argument after liri.js to "command"
-var command = process.argv[2];
+var command 	= process.argv[2];
+
+// Assign argument after "command" to "modifier"
+var modifier 	= process.argv[3];
+
+// CONSOLE LOG CHECKS FOR TWITTER
+// console.log("\n------------------------------------\n");
+// console.log("This is the twitterKey object: ");
+// console.log(twitterKeys);
+// console.log("\n------------------------------------\n");
+
+// console.log("This is the instance of Twitter method 'client':  ");
+// console.log(client);
+// console.log("\n------------------------------------\n");
+
+// console.log("\nThe '" + process.argv[2] + "' command was issued.\n");
 
 
-console.log("\n------------------------------------");
-console.log("This is the twitterKey object: ");
-console.log(twitterKeys);
-console.log("------------------------------------\n");
+// MAIN CONDITIONAL
+// Compound "if-else if" statement which determines which command is being run
 
-console.log("This is the instance of Twitter method 'client':  ");
-console.log(client);
-console.log("------------------------------------\n\n\n\n");
-
-console.log(process.argv[2]);
-
+// "My Tweets" condition calls "client.get" method from Twitter node module
+// which "gets" the top tweets from user: dpkillian
 if (command==="my-tweets"){
-console.log("These are the top tweets from (dpkillian):  \n");
-var params = {screen_name: 'dpkillian'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
+	console.log("These are the top tweets from (dpkillian):  \n");
+	var params = {screen_name: 'dpkillian'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
 
-  	for(i=0; i<tweets.length; i++){
-  		console.log("Tweet #" + i + ": " + tweets[i].text);
-  	}
+	  	// For loop to list out the top tweets, up to a maximum of 20
+	  	for(i=0; (i<tweets.length)&&(i<20); i++){
+	  		console.log("Tweet #" + i + ": " + tweets[i].text);
+	  	}
+	  }
+	});
+
+
+
+// "spotify-this-song" condition calls "client.get" method
+} else if (command==="spotify-this-song"){
+
+spotify.search({ type: 'track', query: modifier }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
   }
+ 
+console.log(data); 
+
+console.log("Artist: \t" 		+ data.artist);
+console.log("Song: \t\t" 		+ data.track);
+console.log("Album: \t\t" 		+ data.album);
+console.log("Preview: \t" 		+ data.preview);
+
+
 });
 
-};
 
 
-
-
-
-
-
-
-
+} else if (command==="movie-this"){
 
 // Then run a request to the OMDB API with the movie specified
-// var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+var queryUrl = "http://www.omdbapi.com/?t=" + modifier + "&y=&plot=short&apikey=trilogy";
 
 // Then create a request to the queryUrl
-// request(queryUrl, function(error, response, body){
+request(queryUrl, function(error, response, body){
 
-//   if (!error && response.statusCode === 200) {
+  if (!error && response.statusCode === 200) {
 
-//     // Then log the body from the site!
-//     console.log("The movie title is: " + JSON.parse(body).Title);
-//     console.log("The release year is: " + JSON.parse(body).Year);    
-//     console.log("The rating: " + JSON.parse(body).Rated);
-//     console.log("The genre is: " + JSON.parse(body).Genre);
-//     console.log("The director is: " + JSON.parse(body).Director);
-//     console.log("The actors are: " + JSON.parse(body).Actors);
-//     console.log("The plot: " + JSON.parse(body).Plot);
-//     console.log("--------------------------------------");
-//     console.log("\n");
-//   }
+    // Then log the body from the site!
+    console.log("The movie title is: " 	+ JSON.parse(body).Title);
+    console.log("The release year is: " + JSON.parse(body).Year);    
+    console.log("The rating: " 			+ JSON.parse(body).Rated);
+    console.log("The genre is: " 		+ JSON.parse(body).Genre);
+    console.log("The director is: " 	+ JSON.parse(body).Director);
+    console.log("The actors are: " 		+ JSON.parse(body).Actors);
+    console.log("The plot: " 			+ JSON.parse(body).Plot);
+    console.log("--------------------------------------");
+    console.log("\n");
+  }
 
-// });
+});
+
+ 
+
+} else if (command==="do-what-it-says"){
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
